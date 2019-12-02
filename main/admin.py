1bb1d0184f94e8388e8c6f3748ae50dd0483632f
Wikan Setiaji <wikansetiaji@gmail.com>
2019-12-02 23:15:26 +0700

    fixing django admin

--- main/admin.py
+++ main/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from .models import Bus
 from .models import GpsPing
 
+@admin.register(Bus)
+class BusAdmin(admin.ModelAdmin):
+    readonly_fields = ["bus_code","trip_id","koridor","timestamp_epoch"]
 
-admin.site.register(Bus)
-admin.site.register(GpsPing)
+@admin.register(GpsPing)
+class BusAdmin(admin.ModelAdmin):
+    readonly_fields = ["bus_code","longitude","latitude","gps_timestamp"]
